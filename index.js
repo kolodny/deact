6e060e4361e5f029e04de7b92a4c5420648e0adf
Moshe Kolodny <kolodny.github@gmail.com>
2016-01-17 14:35:35 -0500

    stop error thrown when placeholder starts with a numver

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-module.exports = noact;
+module.exports = deact;
 
 var forEach = Array.prototype.forEach
 
-function noact(template) {
+function deact(template) {
 
-  var replaceMe = Math.random().toString(36).slice(2);
-  var regex = RegExp(replaceMe + '-(\\d+)');
+  var replaceholder = 'x' + Math.random().toString(36).slice(2);
+  var regex = RegExp(replaceholder + '-(\\d+)');
   var args = arguments;
 
   var innerHTML = template.slice(0, -1).map(function(templateItem, index) {
     var insertItem = args[index + 1];
     if (typeof insertItem === 'function' && templateItem.slice(-1) === '=') {
-      insertItem = '"' + replaceMe + '-' + index + '" x-deact-x';
+      insertItem = '"' + replaceholder + '-' + index + '" x-deact-x';
     }
     return templateItem + insertItem;
   }).join('') + template[template.length - 1];
@@ -21,7 +21,7 @@ function noact(template) {
 
   forEach.call(div.querySelectorAll('[x-deact-x]'), function(element) {
     forEach.call(element.attributes, function(attribute) {
-      var matches = regex.exec(element[attribute.name]);
+      var matches = regex.exec(element.getAttribute(attribute.name));
       if (matches) {
         element[attribute.name] = args[+matches[1] + 1];
       }
@@ -29,7 +29,7 @@ function noact(template) {
   });
 
   if (div.children.length !== 1) {
-    throw new Error('Multiple elements must be wrapped in an enclosing tag');
+    throw new Error('Adjacent elements must be wrapped in an enclosing tag');
   }
   return div.children[0];
 }
